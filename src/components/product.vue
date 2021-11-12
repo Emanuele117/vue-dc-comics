a43633ca1554573d7909cd9a79fe0c9966b25974
Emanuele117 <emanuele.pica7@gmail.com>
2021-11-12 12:06:17 +0100

    inserimento cards in product.vue

---
+++ src/components/product.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="cards">
+    <img :src="image" alt="" />
+    <h3>{{ titolo }}</h3>
+    <p>{{ price }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: String,
+    titolo: String,
+    price: String,
+  },
+};
+</script>
+
+<style>
+</style>
